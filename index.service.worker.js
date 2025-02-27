caff234fed3fda165d9bcbef1a9799a11f34da63
agaAAf <treegleb@gmail.com>
2025-02-27 03:45:46 +0300

    IntroStart

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1740616855|9106386603";
+const CACHE_VERSION = "1740617102|9352802257";
 const CACHE_PREFIX = "Kanye Verse-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
